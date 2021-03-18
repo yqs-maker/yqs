76aa5348d220013a5e22e1e0b41f1587fdc95bbf
YQS <yuqingshan@nuclearmedia.cn>
2021-03-18 15:08:21 +0800

    初始化项目

---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+	<button @click="aaa">按钮</button>
+  </div>
+  
+</template>
+<script>
+	export default {
+		methods:{
+			aaa() {
+				this.$router.replace('/')
+				console.log(this);
+			}
+		}
+	}
+</script>
